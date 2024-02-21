db8c746205d9c6fdb3674a0fee861a5ce102551f
AJ Agers <agersaj@gmail.com>
2024-02-20 20:52:32 -0600

    my length fucntion is working even with an empty list, llprint function is working even with an empty list, happy with results

--- finite.py
+++ finite.py
@@ -3,18 +3,34 @@ from llist import LList, Node, append
 
 def length(lst):
     """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
+    count=0
+    node=lst.head
+    if node!=None:
+        count=1
+        node=node.next
+        while node:
+            count+=1
+            node=node.next
+    print(f'Amount of elements of the list: {count}')
+
 
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
+    node=lst.head
+    if node==None:
+        print('The list is empty, there is no list to print')
+    while node != None:
+        print(node.val, end=' ')
+        node=node.next
+
+    
 
 
 if __name__ == "__main__":
 
-    pass  # delete this line when you add your code below
-
-    #
-    # your tests go here
-    #
+    llist=LList()
+    length(llist)
+    llprint(llist)
+    
+    
